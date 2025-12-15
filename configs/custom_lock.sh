2e23c77ce9018bdcb6d92467bf5d8ae440c07cdb
Igor Ershov <I.Ershov@compassplus.com>
2025-12-15 14:28:21 +0500

    Update v33

---
+++ configs/custom_lock.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export XSECURELOCK_SAVER="saver_protector"
+export XSECURELOCK_AUTH="auth_x11"
+export XSECURELOCK_PASSWORD_PROMPT="default"
+export XSECURELOCK_SHOW_DATETIME="1"
+export XSECURELOCK_DATETIME_FORMAT="%A, %d %B %Y %H:%M:%S"
+export XSECURELOCK_SHOW_KEYBOARD_LAYOUT="1"
+export XSECURELOCK_SHOW_CAPS_LOCK="1"
+export XSECURELOCK_BACKGROUND_IMAGE="/home/iershov/.config/hypr/desk.jpg"
+
+xsecurelock
